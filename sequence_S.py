dd086a6d7195154ce8b97dd6aae670558fa04409
DeepEddy2 <davids.holland@yahoo.com>
2014-08-16 18:50:11 -0500

    Sort_Merge JOIN
    Manifest in query.json, must  be run on OpenStack with ZeroCloud, ZeroVM
    plugins

---
+++ sequence_S.py
@@ -0,0 +1,298 @@
+#!python
+import os
+import math
+import traceback
+import sys
+import cPickle
+import re
+
+inp_dir = '/dev/in'
+
+stdout = '/dev/stdout'
+
+total = 0
+max_count = 0
+
+node_name = os.environ['SCRIPT_NAME']
+
+sequence_count = int(os.environ['SEQ_COUNT'])    #sequence count
+sequenceID = 0
+
+if sequence_count > 1:
+	indexOfDash = node_name.index("-")  #locate index of '-', node ID follows
+	sequenceID = int(node_name[(indexOfDash + 1):])
+
+     
+input_file_names = []
+fileHandleList = []
+#a list of the value at the head of every channel
+listOfChannelTupples = []
+listOfKeys = []
+
+key_col_index = int(os.environ['KEY_COL_INDEX'])
+
+data = []
+
+#DISCOVER LIST OF CHANNELS and OPEN A FILE HANDLE TO EACH
+#returns populated input_file_names, fileHandleList
+def openChannels(files, handles):
+  
+	for inp_file in os.listdir(inp_dir):
+  
+  		#get sorter ID
+		index = inp_file.index("-")
+		sortID = int(inp_file[(index + 1):])
+    
+		#debug
+		#sys.stderr.write(node_name + ": , sortID: " + str(sortID) + '\n')
+    
+	          		
+        # check if sorted is connected to this sequence, if so open file handle
+		if sequence_count == 1:
+			fp = open(os.path.join(inp_dir, inp_file), 'rb', buffering=(2<<18) + 8)
+			handles.append(fp)
+			files.append(inp_file)
+  
+		else:
+			remainder = sortID % sequence_count
+			if ( (remainder == sequenceID) or ((remainder == 0) and (sequenceID == sequence_count))  ) :
+				fp = open(os.path.join(inp_dir, inp_file), 'rb', buffering=(2<<18) + 8)
+				handles.append(fp)
+				files.append(inp_file)
+        
+        
+#INTIALIZE CHANNEL VALUES
+def intialize(handles, tupples, keys):
+	for fp in handles:
+		#value = fp.readline()
+		#tupples.append(value)
+        #keys.append(value[key_col_index])
+		sys.stderr.write('CHANNEL INITIALIZATION: ' + str(fp) + '\n')
+        
+		#tupple = cPickle.load(fp)
+		tupple = fp.readline()
+        
+		#sys.stderr.write(', '.join(tuple) + '\n')
+		sys.stderr.write("tupple: " + str(tupple))
+        
+		tupples.append(tupple)						#store tupple
+		key =  re.split(',',tupple)[key_col_index]
+		sys.stderr.write("key: " + key + '\n')
+		sys.stderr.flush()
+		#keys.append(tupple[key_col_index])			#store key
+		keys.append(key)							#store key
+        
+        
+#DEBUG
+#list values in handles, tupples, keys for corresponding to the current state of the input channels
+def channelState(handles, tupples, keys):
+	sys.stderr.write('Channel State ************\n')
+	i = 0
+	for fp in handles:
+		#sys.stderr.write('Channel' + str(fp) + ': ' + ', '.join(tupples[i]) + '\n')
+		sys.stderr.write('Channel' + str(fp) + '\n')
+		sys.stderr.write("tupple: " + str(tupples[i]))
+		sys.stderr.write('key: ' + keys[i] + '\n')
+		sys.stderr.flush()
+		i += 1
+        
+    
+#DEBUG
+#list inp_files for this sequence/name node      
+def listInputFiles(files):
+	for file in files:
+		sys.stderr.write(node_name + " connected to: " + file + '\n')
+		sys.stderr.flush()
+        
+
+def keyCmp(x,y):
+	result = 0
+	if x.isdigit() and y.isdigit():
+		number1=int(x)
+		number2=int(y)
+		result = cmp(number1,number2)
+	else:
+		result = cmp(x,y)  
+      
+	return result
+  
+  
+class EmptyChannels(Exception):
+	def __init__(self, value):
+		self.value = value
+	def __str__(self):
+		return repr(self.value)
+
+
+channelIndexLastMin = 0	#channel holding min	
+#SEARCH ALL CHANNELS fpr TUPPLE w/ minimum KEY, READ ANOTHER VALUE INTO CHANNEL THAT HELD MIN KEY.
+#returns tupple w/ minimum key
+def getMinValue(handles, tupples, keys):
+ 
+  
+  	#if tupples is empty, all input has been read
+	if len(tupples) == 0 :
+		sys.stderr.write("************Raised EmptyChannels Exception" + '\n')
+		sys.stderr.flush()
+		#sys.exit("**************Channels are empty***************")
+		raise EmptyChannels("all input channels empty\n")
+
+    
+    
+	global channelIndexLastMin
+ 	#locate the min and its index in the list of keys       
+	min = keys[0]       #initialize min to first key		
+	i = 0 				#loop current min index
+	channel = 0			#loop counter
+	for key in keys[1:]:
+      
+		channel += 1
+      	
+		if keyCmp(min,key) > 0:
+			min = key
+			i = channel								#remember which channel min value came from
+		elif keyCmp(min,key) == 0:
+			if  channelIndexLastMin  <> channel:   	#alternate channel when equal keys
+				min = key
+				i = channel							#remember which channel min value came from	   
+                
+     
+	channelIndexLastMin = i   						#reset last index 
+
+    #save tupple to return associated w/ min key
+	min_tupple = tupples[i]
+           
+      
+	#now read in a new value into the channel that held the min, remember a channel's value is 
+    #actually a tupple/row
+	try:  
+		tupples[i] = handles[i].readline()		#read from ith channel
+		#tupples[i] = cPickle.load(handles[i])
+        
+		#sys.exit("getMinValue: step 3 completed")
+        
+		if not tupples[i]:				#this channel is exhausted
+			sys.stderr.write("************* Channel EOF:  " + str(handles[i]) + " ***********************\n")
+			handles[i].close()
+			del handles[i]
+			del tupples[i]
+			del keys[i]
+		else:					#update list of keys at index 
+			keys[i] =  re.split(',',tupples[i])[key_col_index]
+			#keys[i] = (tupples[i])[key_col_index]
+            
+		#sys.exit("getMinValue: step 4 completed")
+        
+	except: # catch *all* exceptions  including EOF
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_traceback)
+		sys.exit("*****************ERROR LOADING TUPPLE or CLOSING CHANNEL*********")
+        
+	#minimum tupple    
+	return	min_tupple
+ 
+
+  
+#                        MAIN EXECUTION SEQUENCE AND LOOP
+#get channels  
+openChannels(input_file_names, fileHandleList)
+
+listInputFiles(input_file_names)   #debug
+
+#init
+intialize(fileHandleList, listOfChannelTupples, listOfKeys)
+
+#debug
+channelState(fileHandleList, listOfChannelTupples, listOfKeys)
+
+
+#open channel to join node
+joinChannel = open('/dev/out/join', 'ab', buffering=(2<<18) + 8)
+
+
+#read channels, extracting and writing tupple with min value to output
+count = 0
+flag = True
+
+while flag:
+  
+  	
+	try:
+      	#get tupple with min key value
+		tupple = getMinValue(fileHandleList, listOfChannelTupples, listOfKeys)
+        
+        #write tuple to join node
+		joinChannel.write(str(tupple))
+        
+		
+		#if ((count % 100) == 0) :
+		#channelState(fileHandleList, listOfChannelTupples, listOfKeys)
+          
+		count += 1
+	except EmptyChannels as e:
+		sys.stderr.write(e.value)
+		sys.stderr.flush()
+		joinChannel.flush()
+		joinChannel.close()
+		flag = False
+	except: # catch *all* exceptions
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_traceback)
+		joinChannel.flush()
+		joinChannel.close()
+		flag = False
+        
+#end while      
+	
+total = count    
+sys.stderr.write("********************Total tupples sequenced: " + str(count))
+sys.stderr.flush()    
+
+
+'''
+for inp_file in os.listdir(inp_dir):
+  
+	index = inp_file.index("-")
+	mapperID = int(inp_file[(index + 1): ])
+    
+	#debug
+	#sys.stderr.write("reducerID: " + str(reducerID) + ", mapperID: " + str(mapperID) + '\n')
+    
+	remainder = mapperID % reducer_count           		
+	# check if mapper is connected to this reducer               
+	if ( (remainder == reducerID) or (remainder == 0 and reducerID == reducer_count)  ) :
+        
+		with open(os.path.join(inp_dir, inp_file), 'rb', -1) as fp:
+			line_count = 0
+        
+			try:  
+          
+			#while True:
+			#	line = fp.readline()
+			#	if not line:
+			#		break
+				#count, filename = line.split()
+              
+				for line in fp:
+					line_count += 1       # count the number of lines in each input file
+					if line_count < 10:
+						sys.stderr.write(str(line))
+		
+			except: # catch *all* exceptions
+				exc_type, exc_value, exc_traceback = sys.exc_info()
+				traceback.print_exception(exc_type, exc_value, exc_traceback)
+			finally:
+				fp.close()
+				if line_count > max_count:
+					max_count = line_count
+				data.append((line_count, inp_file))
+				total += line_count
+
+
+
+for line_count, inp_file in data:
+	print '%d  %s' % (line_count, inp_file)
+    
+'''    
+    
+print '%d  %s' % (total, 'total for: ' + node_name + '\n')
---
+++ sort_S.py
@@ -0,0 +1,144 @@
+#!python
+# -*- coding: utf-8 -*-
+import os
+import csv
+import operator
+import sys
+import traceback
+import cPickle
+import io
+
+#THIS VERSION
+#READING PARAMETER INTO PYTHON SCRIPT FROM JSON FILE USING:  
+#	
+#		Python fragment
+#		os.environ['someParameter']
+#
+#
+#		JSON fragment
+#		"exec": {
+#            "path": "file://python:python",
+#			 "env": {
+#				"someParameter": 4
+#			  }
+#        }
+#
+#
+#ALSO CONSTRUCT WHICH SEQUENCER TO CONNECT TO USING:	os.environ['SCRIPT_NAME']
+#AND A MODULO AND STRING FUNCTIONS TO FORM THE PATH TO A SPECIFIC SEQUENCER USING THE CONNECTION IDS
+#THAT ARE GUARANTEED TO BE PROVIDED BY THE ZEROVM NETWORK LAYER. SEE THE JSON SERVLET DOC
+
+key_col_index = int(os.environ['KEY_COL_INDEX'])
+
+def csv_to_list(csv_file, delimiter=','):
+	""" 
+	Reads in a CSV file and returns the contents as list,
+	where every row is stored as a sublist, and each element
+	in the sublist represents 1 cell in the table.
+  
+	"""
+	#sys.stderr.write(csv_file)
+	with open(csv_file, 'rb', buffering=(2<<18) + 8) as csv_con:
+		reader = csv.reader(csv_con, delimiter=delimiter)
+		csv_list = list(reader)
+		csv_con.close()
+		return csv_list
+      
+	#cmp=lambda x,y: cmp(x.lower(), y.lower())
+    
+def sortCmp(x,y): 
+	if x.isdigit() and y.isdigit():
+		x=str(x)
+		y=str(y)
+	return cmp(x,y)
+      
+def sort_by_column(csv_cont, col, reverse=False):
+	""" 
+	Sorts CSV contents by column name (if col argument is type <str>) 
+	or column index (if col argument is type <int>). 
+    
+	"""
+	header = csv_cont[0]
+	body = csv_cont[1:]
+
+	if isinstance(col, str):  
+		col_index = header.index(col)
+	else:
+		col_index = col
+    
+	#MUCH SLOWER, but does not need:  import operator, not supported in ZeroVM Cloud
+	#body = sorted(body, key =  lambda x :  x[col_index], reverse=reverse)
+    #cmp=lambda x,y: cmp(x.lower(), y.lower())
+	body = sorted(body, cmp=lambda x,y: sortCmp(x,y), key=operator.itemgetter(col_index), reverse=reverse)
+	#body.insert(0, header)  #don't put back CSV header in sorted list, we are done with CSV file writes
+
+	return body
+  
+#create the list from the .csv file, a list of lists, where every line is a list of csv formatted fields         
+csv_list = csv_to_list('/dev/input', ",")
+
+#sort the list by the column key
+csv_list = sort_by_column(csv_list, key_col_index, reverse=False)  # list is sorted
+
+ 
+    
+#output to which sequencer?   get this node's name, strip off its ID number, i.e. for mapper-7 strip off 7
+#then use node's ID number with modulo operator to decide which sequencer to connect to. Get COUNT in JSON file
+#for number of sequencers. For example if "sequence_count":4  then   ID % 4 .  If in this example, ID is 7 then this
+#node should be connected to 7 % 4 = 3 ,   sort_R-7  ---> sequence-3 
+sequence = ""
+sequence_count = int(os.environ['SEQ_COUNT'])
+node_name = os.environ['SCRIPT_NAME']
+
+if sequence_count > 1:
+	indexOfDash = node_name.index("-")  #locate index of '-', node ID follows
+	if (int(node_name[(indexOfDash + 1):]) % sequence_count) == 0:
+		sequence =  "/dev/out/sequence_S-" + str(sequence_count)
+	else:
+		sequence = "/dev/out/sequence_S-" + str(int(node_name[(indexOfDash + 1):]) % sequence_count)
+else:
+	sequence = "/dev/out/sequence_S"
+
+sys.stderr.write("node_name is: " + node_name + ", connected to sequence: " + sequence + '\n')
+sys.stderr.flush();
+#print >> sys.stderr, node_name
+
+with open(sequence, 'ab', buffering=(2<<18) + 8) as fp:
+	try:  
+		line_count = 0
+		for row in csv_list:
+			fp.write(str(row) + '\n')
+			#sys.stderr.write(str(row) + '\n')
+			#sys.stderr.flush()
+			#cPickle.dump(row,fp)
+			#line_count += 1
+            
+			#if line_count > 1:
+			#	raise Exception("DEBUG")
+            
+			#if line_count < 10:
+			#	sys.stderr.write(str(row) + '\n')
+				#sys.stderr.write(', '.join(row) + '\n')
+			#	sys.stderr.flush()
+                
+		sys.stderr.write("*********************EOF***************************")
+		sys.stderr.flush()
+			#print >> fp, '%s' % (line) 
+	except: # catch *all* exceptions
+		exc_type, exc_value, exc_traceback = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_traceback)
+	finally:
+		fp.flush()
+		fp.close()
+        
+     
+		#e = sys.exc_info()[0]
+		#sys.stderr.write(str(e))
+		#print >> sys.stderr, '%s' % (e)
+    
+    # Split off the swift prefix
+    # Just show the container/file
+	#path_info = os.environ['PATH_INFO']
+	#shorter = '/'.join(path_info.split('/')[2:])
+    # Pipe the output to the reducer:
+	#print >>fp, '%d %s' % (len(csv_list), shorter)
